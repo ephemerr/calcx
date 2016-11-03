e00d280ca5cba7446cc2ec1a7c14483d2681f39d
azzel <a.zheleznov@gmail.com>
2016-11-03 17:24:44 +0300

    betha

--- calcx.cpp
+++ calcx.cpp
@@ -1,33 +1,37 @@
 #include "calcx.h"
 #include "ui_calcx.h"
+#include "assert.h"
 
-#include <QPushButton>
 #include <QKeyEvent>
+#include <QWidget>
 
 Calcx::Calcx(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Calcx)
 {
     ui->setupUi(this);
-    create_button(QString("7"),static_cast<QLayout*>(ui->horizontalLayout_1));
-    create_button(QString("8"),static_cast<QLayout*>(ui->horizontalLayout_1));
-    create_button(QString("9"),static_cast<QLayout*>(ui->horizontalLayout_1));
-    create_button(QString("/"),static_cast<QLayout*>(ui->horizontalLayout_1));
+    create_button(QString("7"),ui->horizontalLayout_1);
+    create_button(QString("8"),ui->horizontalLayout_1);
+    create_button(QString("9"),ui->horizontalLayout_1);
+    create_button(QString("/"),ui->horizontalLayout_1);
 
-    create_button(QString("4"),static_cast<QLayout*>(ui->horizontalLayout_2));
-    create_button(QString("5"),static_cast<QLayout*>(ui->horizontalLayout_2));
-    create_button(QString("6"),static_cast<QLayout*>(ui->horizontalLayout_2));
-    create_button(QString("*"),static_cast<QLayout*>(ui->horizontalLayout_2));
+    create_button(QString("4"),ui->horizontalLayout_2);
+    create_button(QString("5"),ui->horizontalLayout_2);
+    create_button(QString("6"),ui->horizontalLayout_2);
+    create_button(QString("*"),ui->horizontalLayout_2);
 
-    create_button(QString("1"),static_cast<QLayout*>(ui->horizontalLayout_3));
-    create_button(QString("2"),static_cast<QLayout*>(ui->horizontalLayout_3));
-    create_button(QString("3"),static_cast<QLayout*>(ui->horizontalLayout_3));
-    create_button(QString("-"),static_cast<QLayout*>(ui->horizontalLayout_3));
+    create_button(QString("1"),ui->horizontalLayout_3);
+    create_button(QString("2"),ui->horizontalLayout_3);
+    create_button(QString("3"),ui->horizontalLayout_3);
+    create_button(QString("-"),ui->horizontalLayout_3);
 
-    create_button(QString("0"),static_cast<QLayout*>(ui->horizontalLayout_4));
-    create_button(QString("."),static_cast<QLayout*>(ui->horizontalLayout_4));
-    create_button(QString("="),static_cast<QLayout*>(ui->horizontalLayout_4));
-    create_button(QString("+"),static_cast<QLayout*>(ui->horizontalLayout_4));
+    create_button(QString("0"),ui->horizontalLayout_4);
+    create_button(QString("."),ui->horizontalLayout_4);
+    QPushButton * butt_res =
+    create_button(QString("="),ui->horizontalLayout_4);
+    create_button(QString("+"),ui->horizontalLayout_4);
+
+    connect( butt_res, SIGNAL(clicked()), this, SLOT(on_lineEdit_returnPressed()) );
 }
 
 Calcx::~Calcx()
@@ -39,13 +43,15 @@ Calcx::~Calcx()
     delete ui;
 }
 
-void Calcx::create_button(QString str, QLayout *parent)
+QPushButton * Calcx::create_button(QString str, QLayout *parent)
 {
     QPushButton *butt = new QPushButton();
     parent->addWidget(butt);
     butt->setText(str);
+    butt->setObjectName(QString("butt_") + str);
     button_prints.insert(butt,str);
     connect (butt, SIGNAL(clicked()), this, SLOT(on_print_clicked()));
+    return butt;
 }
 
 void Calcx::on_print_clicked()
@@ -55,6 +61,7 @@ void Calcx::on_print_clicked()
 }
 
 int test_char(const QString &str, int i) {
+    if (i >= str.length()) return 0;
     char c = str[i].toLatin1();
     switch (c) {
     case '0':
@@ -73,22 +80,51 @@ int test_char(const QString &str, int i) {
     return 0; // 0 at operator and >0(size) at operand
 }
 
-int op_priority(char op){
-    int res=0;
+inline double mult (double a, double b) {
+    return a*b;
+}
+inline double div (double a, double b) {
+    return a/b;
+}
+inline double sum (double a, double b) {
+    return a+b;
+}
+inline double min (double a, double b) {
+    return a-b;
+}
+typedef double (*Arithmetic)(double, double);
+
+enum {
+    CALC_OP_NONE,
+    CALC_OP_SUM,
+    CALC_OP_MIN,
+    CALC_OP_MULT,
+    CALC_OP_DIV,
+    CALC_OP_UNKNOWN
+};
+
+struct Operation {
+    int priority;
+    Arithmetic calc;
+} operations[CALC_OP_UNKNOWN] = {
+    {0,NULL},
+    {1, sum},
+    {1, min},
+    {2, mult},
+    {2, div}
+};
+
+int op_bind(char op){
     switch (op) {
-        case 0: break;
-        case '+':
-        case '-':
-            res = 1;
-            break;
-        case '*':
-        case '/':
-            res = 2;
-            break;
+        case '+': return CALC_OP_SUM;
+        case '-': return CALC_OP_MIN;
+        case '*': return CALC_OP_MULT;
+        case '/': return CALC_OP_DIV;
     }
-    return res;
+    return CALC_OP_NONE;
 }
 
+/// Convert string to reverse polish notation (RPN) list of lexems
 QList<Lexem> polish(const QString& str) {
     int len = str.length();
     QList<Lexem> res;
@@ -96,79 +132,70 @@ QList<Lexem> polish(const QString& str) {
     for (int i=0; i < len; i++)
     {
         int operand = test_char(str,i);
-        char operator_ = operand ? 0 : str[i].toLatin1();
+        char operator_id = operand ? 0 : op_bind(str[i].toLatin1());
         int len = operand ? operand : 1;
-        Lexem lexem = Lexem(str.mid(i,len),operator_);
-        if (operator_) {
+        Lexem lexem = Lexem(str.mid(i,len),operator_id);
+        i += len-1;
+        if (operator_id) {
             while (station.size()) {
                 Lexem &top = station.top();
-                if (op_priority( operator_) <= op_priority(top.second)) {
+                if (operations[operator_id].priority <=
+                        operations[top.second].priority) {
                     res.append(station.pop());
                 } else {
-                    station.push(lexem);
                     break;
                 }
             }
+            station.push(lexem);
         } else {
             res.append(lexem);
         }
     }
+    while (station.size()) {
+        res.append(station.pop());
+    }
+    return res;
 }
 
 QString lexem_print(QList<Lexem> to_print) {
     QString res = "";
     foreach (Lexem lexem, to_print) {
         res += lexem.first;
+        if (!lexem.second) {
+            res += ",";
+        }
     }
     return res;
 }
 
-double Calcx::parse()
+double Calcx::calc(QList<Lexem> &rpn, int &i)
 {
-    double res = 0;
-    return res;
-//    bool sign = false;
-//    do {
-//       char c = str[i];
-//       switch (c) {
-//       case '-':
-//           if (!i) {
-//               sign = true;
-//               continue;
-//           } else {
-//               return  sign*res - parse(str+i+1, len-i-1);
-//           }
-//       case '+':
-//           if (!i) {
-//               continue;
-//           } else {
-//               return  sign*res + parse(str+i+1, len-i-1);
-//           }
-//       case '0':
-//       case '1':
-//       case '2':
-//       case '3':
-//       case '4':
-//       case '5':
-//       case '6':
-//       case '7':
-//       case '8':
-//       case '9':
-//           res += c - '0';
-//       }
-//       if (c <= '9' && c >=0) {
-//           double res
-//       }
-//    }
+    Lexem &current = rpn[i];
+    i--;
+    if (current.second == CALC_OP_NONE) {
+        double res = current.first.toDouble();
+        return res;
+    } else {
+        double a = calc(rpn,i);
+        double b = calc(rpn,i);
+        Arithmetic func = operations[current.second].calc;
+        return func(b,a);
+    }
 }
 
-void Calcx::keyPressEvent(QKeyEvent * event)
+void Calcx::on_lineEdit_returnPressed()
 {
-    switch (event->key()) {
-        case Qt::Key_Enter:
-
-            break;
-        default:
-          QWidget::keyPressEvent(event);
-    }
+    QString exp = QString("[0-9-+*/.]+");
+    QValidator *valid = new QRegExpValidator(QRegExp(exp), this);
+    QString formula = ui->lineEdit->text();
+    formula.replace(" ","");
+    formula.replace("=","");
+    int pos = 0;
+    if (valid->validate(formula,pos) !=  QValidator::Acceptable)
+        return;
+    QList<Lexem> pol = polish(formula);
+    QString rpn = lexem_print(pol);
+    int i = pol.size()-1;
+    double res = calc(pol, i);
+    ui->lineEdit->setText(QString::number(res));
 }
--- calcx.h
+++ calcx.h
@@ -3,26 +3,28 @@
 
 #include <QMainWindow>
 #include <QtCore>
+#include <QPushButton>
 
 namespace Ui {
 class Calcx;
 }
 
-typedef QPair<QString, bool> Lexem;
+typedef QPair<QString, char> Lexem;
+typedef QList<Lexem>::iterator Iterator;
 
 class Calcx : public QMainWindow
 {
     Q_OBJECT
     QMap<QObject*, QString> button_prints;
-    void create_button(QString str, QLayout *parent);
+    QPushButton* create_button(QString str, QLayout *parent);
+    double calc(QList<Lexem> &rpn, int &i);
 public:
     explicit Calcx(QWidget *parent = 0);
     ~Calcx();
 
 private slots:
     void on_print_clicked();
-    double parse();
-    void keyPressEvent(QKeyEvent * event);
+    void on_lineEdit_returnPressed();
 
 private:
     Ui::Calcx *ui;
